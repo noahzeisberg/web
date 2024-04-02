e1a625b66baf643137cf4515a33ce4f976b911fb
NoahOnFyre <noahonfyre@gmail.com>
2024-04-02 14:36:25 +0200

    chore: update config.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  modules: ['@nuxt/ui', '@nuxtjs/tailwindcss'],
+  app: {
+    head: {
+      title: "Nyronium Network",
+      link: [{ rel: 'icon', type: 'image/ico', href: 'data:image/svg+xml,%3Csvg viewBox=\'0 0 24 24\' fill=\'none\' xmlns=\'http://www.w3.org/2000/svg\' class=\'size-12\'%3E%3Cpath fill-rule=\'evenodd\' clip-rule=\'evenodd\' d=\'M11 8L3 16H11L11 8ZM13 16L21 8L13 8V16Z\' fill=\'white\'/%3E%3C/svg%3E' }],
+      meta: [{ name: "referrer", content: "no-referrer" }]
+    },
+  }
 })
